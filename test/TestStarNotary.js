f6592406d7c0d92990ce1f5e5af4362493a904d6
Pranit-c <60237284+Pranit-c@users.noreply.github.com>
2023-03-05 23:08:51 -0500

    Update TestStarNotary.js

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -64,13 +64,15 @@ it('lets user2 buy a star and decreases its balance in ether', async() => {
     let user2 = accounts[2];
     let starId = 5;
     let starPrice = web3.utils.toWei(".01", "ether");
-    let balance = web3.utils.toWei(".05", "ether");
+    let balance = web3.utils.toWei(".1", "ether");
     await instance.createStar('awesome star', starId, {from: user1});
     await instance.putStarUpForSale(starId, starPrice, {from: user1});
     await instance.approve(user2, starId, {from: user1});
-    let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user2);
+    let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user1);
     const balanceOfUser2BeforeTransaction = await web3.eth.getBalance(user2);
-    await instance.buyStar(starId, {from: user2, value: balance, gasPrice: 0, maxFeePerGas: web3.utils.toWei('20', 'gwei')});
+    const gasPrice = await web3.eth.getGasPrice();
+    const maxFeePerGas = parseInt(gasPrice) * 2; // set max fee to 2x the current gas price
+    await instance.buyStar(starId, {from: user2, value: balance, maxFeePerGas: maxFeePerGas});
     const balanceAfterUser2BuysStar = await web3.eth.getBalance(user2);
     let value = Number(balanceOfUser2BeforeTransaction) - Number(balanceAfterUser2BuysStar);
     assert.equal(value, starPrice);
